6e41b1997eb44495c8d52e1cdea36a0041f0cac8
Franklin <angelguevaraj@gmail.com>
2021-12-30 18:53:49 -0600

    Agregar archivos de proyecto.

---
+++ APIdeImagenes/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace APIdeImagenes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.img = new System.Windows.Forms.PictureBox();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.img)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img
+            // 
+            this.img.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.img.Location = new System.Drawing.Point(136, 27);
+            this.img.Name = "img";
+            this.img.Size = new System.Drawing.Size(528, 314);
+            this.img.TabIndex = 0;
+            this.img.TabStop = false;
+            this.img.Click += new System.EventHandler(this.img_Click);
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnConsulta.Location = new System.Drawing.Point(117, 372);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(100, 42);
+            this.btnConsulta.TabIndex = 1;
+            this.btnConsulta.Text = "Consultar Imagen";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            this.btnConsulta.Click += new System.EventHandler(this.btnConsulta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.img);
+            this.Name = "Form1";
+            this.Text = "Consumo de imagenes";
+            ((System.ComponentModel.ISupportInitialize)(this.img)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox img;
+        private System.Windows.Forms.Button btnConsulta;
+    }
+}
+
---
+++ APIdeImagenes/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace APIdeImagenes
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bitmap;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnConsulta_Click(object sender, EventArgs e)
+        {
+
+            string url = "https://source.unsplash.com/random";
+
+            //if (!CheckConnection(url)) MessageBox.Show("No hay conexion");
+            
+                try
+                {
+                    WebClient client = new WebClient();
+                    Stream stream = client.OpenRead(url);
+                    bitmap = new Bitmap(stream);
+                    stream.Flush();
+                    stream.Close();
+                }
+                catch (Exception a)
+                {
+                    Console.WriteLine(a.Message);
+                }
+                img.BackgroundImage = bitmap;
+            
+            
+        }
+
+        private static bool CheckConnection(String URL)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+                request.Timeout = 5000;
+                request.Credentials = CredentialCache.DefaultNetworkCredentials;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                    return true;
+                else
+                    return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void img_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
