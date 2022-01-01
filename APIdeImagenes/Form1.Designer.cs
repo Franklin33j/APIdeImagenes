07937e4483f16d6e1660bc1e7c3789ab5e5445a9
Franklin <angelguevaraj@gmail.com>
2021-12-31 20:48:30 -0600

    version inicial de Aplicacion Winforms que consume API de imagenes

--- APIdeImagenes/Form1.Designer.cs
+++ APIdeImagenes/Form1.Designer.cs
@@ -30,50 +30,80 @@ namespace APIdeImagenes
         private void InitializeComponent()
         {
             this.img = new System.Windows.Forms.PictureBox();
-            this.btnConsulta = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnImagen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.img)).BeginInit();
             this.SuspendLayout();
             // 
             // img
             // 
             this.img.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.img.BackColor = System.Drawing.SystemColors.HighlightText;
             this.img.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.img.Location = new System.Drawing.Point(136, 27);
             this.img.Name = "img";
             this.img.Size = new System.Drawing.Size(528, 314);
             this.img.TabIndex = 0;
             this.img.TabStop = false;
-            this.img.Click += new System.EventHandler(this.img_Click);
             // 
-            // btnConsulta
+            // btnGuardar
             // 
-            this.btnConsulta.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnConsulta.Location = new System.Drawing.Point(117, 372);
-            this.btnConsulta.Name = "btnConsulta";
-            this.btnConsulta.Size = new System.Drawing.Size(100, 42);
-            this.btnConsulta.TabIndex = 1;
-            this.btnConsulta.Text = "Consultar Imagen";
-            this.btnConsulta.UseVisualStyleBackColor = true;
-            this.btnConsulta.Click += new System.EventHandler(this.btnConsulta_Click);
+            this.btnGuardar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnGuardar.Location = new System.Drawing.Point(301, 372);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(95, 41);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar Imagen";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnImagen
+            // 
+            this.btnImagen.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnImagen.Location = new System.Drawing.Point(145, 372);
+            this.btnImagen.Name = "btnImagen";
+            this.btnImagen.Size = new System.Drawing.Size(121, 41);
+            this.btnImagen.TabIndex = 0;
+            this.btnImagen.Text = "Mostrar Imagen";
+            this.btnImagen.UseVisualStyleBackColor = true;
+            this.btnImagen.Click += new System.EventHandler(this.btnImagen_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(574, 428);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Creditos para:  https://source.unsplash.com";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnImagen);
+            this.Controls.Add(this.btnGuardar);
             this.Controls.Add(this.img);
             this.Name = "Form1";
             this.Text = "Consumo de imagenes";
             ((System.ComponentModel.ISupportInitialize)(this.img)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox img;
-        private System.Windows.Forms.Button btnConsulta;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnImagen;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- APIdeImagenes/Form1.cs
+++ APIdeImagenes/Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using System.Web;
 using System.Net;
 using System.IO;
+using System.Net.NetworkInformation;
+using System.Drawing.Imaging;
 
 namespace APIdeImagenes
 {
@@ -22,52 +24,75 @@ namespace APIdeImagenes
 
         }
 
-        private void btnConsulta_Click(object sender, EventArgs e)
-        {
-
-            string url = "https://source.unsplash.com/random";
-
-            //if (!CheckConnection(url)) MessageBox.Show("No hay conexion");
-            
-                try
-                {
-                    WebClient client = new WebClient();
-                    Stream stream = client.OpenRead(url);
-                    bitmap = new Bitmap(stream);
-                    stream.Flush();
-                    stream.Close();
-                }
-                catch (Exception a)
-                {
-                    Console.WriteLine(a.Message);
-                }
-                img.BackgroundImage = bitmap;
-            
-            
-        }
-
-        private static bool CheckConnection(String URL)
+        //Recupera la imagen de la API
+        private async Task ObtenerImagen(string url)
         {
             try
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
-                request.Timeout = 5000;
-                request.Credentials = CredentialCache.DefaultNetworkCredentials;
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                if (response.StatusCode == HttpStatusCode.OK)
-                    return true;
-                else
-                    return false;
+                btnImagen.Enabled = false;
+                WebClient client = new WebClient();
+                Stream stream = await client.OpenReadTaskAsync(url);
+                bitmap = new Bitmap(stream);
+                stream.Flush();
+                stream.Close();
+               
             }
-            catch
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message+" Error ");
+            }
+ 
+        }
+
+        private async Task<bool> CheckConnection()
+        {
+            WebClient client = new WebClient();
+            try
+            {
+                Uri url = new Uri("http://www.google.com");
+                using (await client.OpenReadTaskAsync(url))
+                {
+                    return true;
+                }      
+            }
+            catch (WebException)
             {
                 return false;
             }
         }
 
-        private void img_Click(object sender, EventArgs e)
+        private void btnGuardar_Click(object sender, EventArgs e)
         {
+            var a= img.BackgroundImage;
+            if (a != null) {
+                
+                Random ran = new Random();
+                int aleatorio= ran.Next(0, 9);
+                saveFileDialog1.FileName = "imagen"+aleatorio;
+                saveFileDialog1.Filter = "Images|*.jpg; *.pgn";
+                
+                ImageFormat formato = ImageFormat.Jpeg;
+                DialogResult result = saveFileDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    bitmap.Save(saveFileDialog1.FileName, formato);
+                }
+            } 
+            else MessageBox.Show("No has buscado ninguna imagen");
+        }
+
+        private async void btnImagen_Click(object sender, EventArgs e)
+        {
+            string url = "https://source.unsplash.com/random";
+            bool conexion = await CheckConnection();
+            if (conexion)
+            {
+                await ObtenerImagen(url);
+                img.BackgroundImage = bitmap;
+                btnImagen.Enabled = true;
+            }
+            else MessageBox.Show("No hay conexion a internet", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
         }
     }
 }
